249083ba3aa799fb295f9b034f6520671a036b07
Joshua Almodovar <joshuarosamktp4@gmail.com>
2021-09-07 15:54:35 -0400

    create tokens

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,4 +1,6 @@
 const { User, Thought } = require("../models");
+const { AuthencationError } = require("apollo-server-express");
+const { signToken } = require("../utils/auth");
 
 const resolvers = {
   Query: {
@@ -20,6 +22,30 @@ const resolvers = {
     },
     thought: async (parent, { _id }) => {
       return Thought.findOne({ _id });
+    },
+  },
+
+  Mutation: {
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthencationError("Incorrect credentials");
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthencationError("Incorrect credentials");
+      }
+
+      const token = signToken(user);
+      return { token, user };
     },
   },
 };
